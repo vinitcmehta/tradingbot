bdae02c55a28ff569b05f56a45dba3d9e2cd96b1
vinitcmehta <vinit_m@hotmail.co.uk>
2020-08-04 18:21:53 +0100

    Adding pseudocode for bot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import config
 #https://github.com/sammchardy/python-binance
 from binance.client import Client
+import time
+import constant
 
 api_key = config.api_key
 api_secret = config.api_secret
@@ -11,4 +13,46 @@ client = Client(api_key, api_secret)
 depth = client.get_order_book(symbol='BNBBTC')
 
 print(depth)
+
+#Helper functions
+
+def get_balances():
+    print("Getting balances")
+
+def get_market_price():
+    print("Getting market price")
+
+def place_sell_order():
+    print("place sell order")
+
+def place_buy_order():
+    print("place buy order")
+
+def get_operation_details():
+    print("get operation details")
+
+#Decision making
+
+isNextOperationBuy = True
+
+lastOpPrice = 100.00
+
+def attempt_to_make_trade():
+    print ("attempt to make a trade")
+
+def try_to_buy():
+    print ("try to buy")
+    isNextOperationBuy = False
+
+def try_to_sell():
+    print ("try to sell")
+    isNextOperationBuy = True
+
+
+#Main loop
+
+while True:
+    attempt_to_make_trade()
+    time.sleep(30)
+
 
